91280471e7ce9f1c8e1eeec4b0a96b5b7f38c170
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2019-06-13 19:06:08 +1000

    Fix encoding issue, use UTF-8
    See https://travis-ci.org/gonzalo-bulnes/kata-python-web-app/jobs/545142368#L421
    and https://stackoverflow.com/a/52910328/3767264

--- test_numbersandcolors.py
+++ test_numbersandcolors.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 import os
 from test.support import EnvironmentVarGuard
 import unittest
@@ -8,21 +10,21 @@ class TestNumbersAndColors(unittest.TestCase):
 
     def test_lucky_number_is_an_integer(self):
         lucky_number = numbersandcolors.lucky_number()
-        self.assertIsInstance(lucky_number, int, "expected lucky_number to return an integer") 
+        self.assertIsInstance(lucky_number, int, "expected lucky_number to return an integer")
 
     def test_lucky_color_is_a_string(self):
         lucky_color = numbersandcolors.lucky_color()
-        self.assertIsInstance(lucky_color, str, "expected lucky_color to return a string") 
+        self.assertIsInstance(lucky_color, str, "expected lucky_color to return a string")
 
     def test_lucky_color_defaults_to_purple(self):
         lucky_color = numbersandcolors.lucky_color()
-        self.assertEqual(lucky_color, 'purple', "expected default lucky_color to be 'purple'") 
+        self.assertEqual(lucky_color, 'purple', "expected default lucky_color to be 'purple'")
 
     def test_lucky_color_is_read_from_environemt(self):
         self.env = EnvironmentVarGuard()
         self.env.set('COLOR', 'green')
         lucky_color = numbersandcolors.lucky_color()
-        self.assertEqual(lucky_color, 'green', "expected lucky_color to be 'green'") 
+        self.assertEqual(lucky_color, 'green', "expected lucky_color to be 'green'")
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestNumbersAndColors)
