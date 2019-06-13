d6de93bf5e7041d61bef0022aacb9237d243d59c
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2019-06-13 17:49:16 +1000

    Add application :)

---
+++ numbersandcolors.py
@@ -0,0 +1,16 @@
+import os
+import random
+
+from flask import Flask
+
+app = Flask(__name__)
+
+def lucky_color():
+    return os.getenv('COLOR') or "purple"
+
+def lucky_number():
+    return random.randint(1,7)
+
+@app.route('/')
+def index():
+    return "Your lucky colored number is %s — %s!" % (lucky_number(), lucky_color())
